98a4779ce2c2f7e7ea57d2393392176593bb3295
Tada Masahiro <tadamasahiro@TadanoMacBook-Air.local>
2026-03-16 16:05:22 +0900

    fix: support APP_S3_KEY/APP_S3_SECRET env vars for Amplify

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -8,8 +8,8 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 const s3 = new S3Client({
   region: process.env.S3_BUCKET_REGION ?? "ap-northeast-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: (process.env.APP_S3_KEY ?? process.env.AWS_ACCESS_KEY_ID)!,
+    secretAccessKey: (process.env.APP_S3_SECRET ?? process.env.AWS_SECRET_ACCESS_KEY)!,
   },
 });
 
